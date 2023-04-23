ef8757dea083987318cd6892b8e6458922d73c35
于星魁 <yuxingkui@yuxingkuideMacBook-Pro-2.local>
2023-04-23 15:32:35 +0800

    封装

--- src/Redis.php
+++ src/Redis.php
@@ -17,60 +17,13 @@ use Hyperf\Utils\ApplicationContext;
 class Redis
 {
 
-	public static function __callStatic($name, $arguments)
-	{
-		$container = ApplicationContext::getContainer();
-		$redis     = $container->get(\Redis::class);
+    public static function __callStatic($name, $arguments)
+    {
+        $container = ApplicationContext::getContainer();
 
-		if (is_string($arguments[0])) {
-			$arguments[0] = config('redis.prefix', '') . $arguments[0];
-		} elseif (is_array($arguments[0])) {
-			foreach ($arguments[0] as &$key) {
-				$key = config('redis.prefix', '') . $key;
-			}
-		}
+        $redis = $container->get(\Hyperf\Redis\Redis::class);
 
-		return $redis->$name(...$arguments);
-	}
-
-	public function __call($name, $arguments)
-	{
-		$container = ApplicationContext::getContainer();
-		$redis     = $container->get(\Redis::class);
-
-		if (! in_array($name, ['eval'])) {
-			if (is_string($arguments[0])) {
-				$arguments[0] = config('redis.prefix', '') . $arguments[0];
-			} elseif (is_array($arguments[0])) {
-				foreach ($arguments[0] as &$key) {
-					$key = config('redis.prefix', '') . $key;
-				}
-			}
-		}
-		return $redis->$name(...$arguments);
-	}
-
-	/**
-	 * @param string $key
-	 * @param int $timeout
-	 * @return string|null
-	 */
-	public static function pop(string $key, int $timeout)
-	{
-		$data = Redis::brPop($key, $timeout);
-
-		return count($data) ? $data[1] : null;
-	}
-
-	/**
-	 *
-	 * @param string $key
-	 * @param string|mixed $value
-	 * @return mixed
-	 */
-	public static function push($key, ...$value)
-	{
-		return Redis::lpush($key, ...$value);
-	}
+        return $redis->{$name}(...$arguments);
+    }
 
 }
--- src/ToolHelper.php
+++ src/ToolHelper.php
@@ -126,16 +126,4 @@ class ToolHelper
     {
         return system("cat /etc/hosts | grep `hostname` | awk '{print $1}'") ?: "127.0.0.1";
     }
-
-    /**
-     * 驼峰转下划线
-     * @param string $camelCaps
-     * @param string $separator
-     * @return string
-     */
-    public static function unCamelize(string $camelCaps, string $separator = '_'): string
-    {
-        return strtolower(preg_replace('/([a-z])([A-Z])/', "$1".$separator."$2", $camelCaps));
-    }
-
 }
